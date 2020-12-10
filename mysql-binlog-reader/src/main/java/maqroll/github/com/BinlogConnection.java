db7429bbbc86ecb8f116344b523579589b39fdab
maqroll <loteroc@gmail.com>
2020-12-10 08:10:54 +0100

    .

--- mysql-binlog-reader/src/main/java/maqroll/github/com/BinlogConnection.java
+++ mysql-binlog-reader/src/main/java/maqroll/github/com/BinlogConnection.java
@@ -1,9 +1,16 @@
 package maqroll.github.com;
 
 import com.github.mheath.netty.codec.mysql.CapabilityFlags;
+import com.github.mheath.netty.codec.mysql.Handshake;
 import com.github.mheath.netty.codec.mysql.MysqlClientPacketEncoder;
 import com.github.mheath.netty.codec.mysql.MysqlServerConnectionPacketDecoder;
+import com.github.mheath.netty.codec.mysql.MysqlServerPacket;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -38,5 +45,30 @@ public class BinlogConnection {
             ch.pipeline().addLast(new MysqlClientPacketEncoder());
           }
         });
+
+    ChannelFuture connectFuture = bootstrap.connect("localhost", 3306);
+
+    try {
+      connectFuture.addListener((ChannelFutureListener) future -> {
+        if (future.isSuccess()) {
+          future.channel().pipeline().addLast(new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+              if (msg instanceof Handshake) {
+                CapabilityFlags.getCapabilitiesAttr(ctx.channel()).retainAll(((Handshake) msg).getCapabilities());
+              }
+              //serverPackets.add((MysqlServerPacket) msg);
+            }
+          });
+        }
+      });
+      connectFuture = connectFuture.sync();
+      if (!connectFuture.isSuccess()) {
+        throw new RuntimeException(connectFuture.cause());
+      }
+      Channel channel = connectFuture.channel();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
